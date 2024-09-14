0dbb01fc84a967adfcecb303ccbb737c8e4c116b
jadj <jakub.jadczyk@innsoft.pl>
2024-09-14 12:43:12 +0200

    show list

--- next-project/src/app/(private)/dashboard/page.tsx
+++ next-project/src/app/(private)/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from 'next/navigation';
 import Form from './Form';
+import FileList from './FileList';
 
 import { createClientServer } from '@lib/db';
 
@@ -15,8 +16,9 @@ export default async function PrivatePage() {
     <div>
       <h1>Good to see you</h1>
       <p> {data.user.email}</p>
-
+      
       <Form />
+      <FileList />
     </div>
   );
 }
